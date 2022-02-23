d3b2e6920d113cb1261fb473df8577bebec94c71
HayleyHaggard <nastena_panova2000@mail.ru>
2022-02-23 13:34:55 +0300

    added contacts

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,15 @@
 $(function () {
 
 
+  let map;
+
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+  }
+
 
   $('.product-tabs__top-item').on('click', function (e) {
     e.preventDefault();
